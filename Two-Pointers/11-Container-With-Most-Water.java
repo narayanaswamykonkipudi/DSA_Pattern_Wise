1704c0f10f116f11d479def475f0a6f0ea077057
NARAYANA SWAMY K <konkipudinarayanaswamy@gmail.com>
2026-09-10 10:41:54 +0530

    [11] [Time Beats: 82.94%] [Memory Beats: 98.14%] - LeetPush

--- Two-Pointers/11-Container-With-Most-Water.java
+++ Two-Pointers/11-Container-With-Most-Water.java
@@ -5,18 +5,35 @@ class Solution {
         int r = n-1;
         int left_max = Integer.MIN_VALUE;
         int right_max = Integer.MIN_VALUE;
-        int idx1 = -1;
-        int idx2 = -1;
         int ans = 0;
         while(l<r){
-            left_max = Math.max(left_max,height[l]);
-            right_max = Math.max(right_max,height[r]);
-            idx1 = l;
-            idx2 = r;
-            ans = Math.max(ans,(idx2-idx1)*Math.min(right_max,left_max));
+            ans = Math.max(ans,(r-l)*Math.min(height[l],height[r]));
             if(height[l]>height[r]) r--;
             else l++;
         }
         return ans;
     }
 }
+
+/*
+#Approach 
+.Two pointers
+
+To calculate the most water presented container 
+
+i have intitilized the two pointers starting and ending which are l=0 and r=n-1
+
+for every iteration i need to calculate the ans which is maximum water conatiner
+by using the (right idx-left idx)*Math.min(height[l],height[r]).
+
+the left pointer moves when it is less that than the right max.
+the right pointer decreases when it is less than the left max.
+
+finally the maximum water with presented in the container will calculated.
+
+worst-case:
+
+Time Complexity - O(n)
+Space Complexity - O(1)
+
+*/
