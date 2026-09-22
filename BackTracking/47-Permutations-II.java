45c87bd4869e47ff87bca20f728531ac7b56d423
NARAYANA SWAMY K <konkipudinarayanaswamy@gmail.com>
2026-09-22 10:02:53 +0530

    [47] [Time Beats: 10.81%] [Memory Beats: 12.06%] - LeetPush

---
+++ BackTracking/47-Permutations-II.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static Set<List<Integer>> res = new HashSet<>();
+    public void solve(int[] nums,boolean[] v,ArrayList<Integer> curr,int n){
+        if(curr.size()==n){
+            res.add(new ArrayList<Integer>(curr));
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(!v[i]){
+                v[i] = true;
+                curr.add(nums[i]);
+                solve(nums,v,curr,n);
+                v[i] = false;
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res.clear();
+        int n = nums.length;
+        boolean visited[] = new boolean[n];
+        solve(nums,visited,new ArrayList<Integer>(),n);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(List<Integer> a : res){
+            ans.add(new ArrayList<>(a));
+        }
+        return ans;
+    }
+}
